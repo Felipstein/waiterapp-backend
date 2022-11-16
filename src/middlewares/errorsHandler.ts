eccf85e0e263eab331dff30602d4207156df1882
Felipstein <luisfelipe-oliveira@outlook.com.br>
2022-11-16 01:24:51 -0300

    feat: added console output in error handler

--- src/middlewares/errorsHandler.ts
+++ src/middlewares/errorsHandler.ts
@@ -3,5 +3,12 @@ import { NextFunction, Request, Response } from 'express';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function errorHandlers(err: Error, req: Request, res: Response, next: NextFunction) {
 
+  console.log('#### ERROR HANDLER ####');
+  if (err.stack) {
+    console.error(err.stack);
+  } else {
+    console.error(`${err.name}: ${err.message}`);
+  }
+
   return res.status(500).json({ message: 'Internal server error' });
 }
