f3a72e240b91404db9b267abc88e4b659b9e6bcb
Felipstein <luisfelipe-oliveira@outlook.com.br>
2022-11-16 02:36:21 -0300

    fix: list orders returning products info

--- src/useCases/orders/listOrders.ts
+++ src/useCases/orders/listOrders.ts
@@ -3,7 +3,7 @@ import { Request, Response } from 'express';
 import { Order } from '../../models/Order';
 
 export async function listOrders(req: Request, res: Response) {
-  const orders = await Order.find();
+  const orders = await Order.find().populate('products.product');
 
   return res.json(orders);
 }
